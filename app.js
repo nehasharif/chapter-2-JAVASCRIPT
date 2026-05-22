a6eb8a9e847f9dc377eb571064535aa6ac55d613
nehasharif <nehasharif529@gmail.com>
2026-05-23 02:06:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+letusername = "Neha";
+
+letmyName = "Noor";
+
+letmessage = "Hello World";
+
+letage = 22;
+
+letstudentsBio = "Master At KU";
+
+letpizza = "PIZZA\nPIZZ\nΡΙΖ\nPI\nP";
+
+letemail = "example@example.com";
+
+letbook = "A way to learn JavaScript";
+
+alert(username + ""+ myName);
+
+alert('${age} Years old');
+
+alert(message);
+
+alert(studentsBio);
+
+alert(pizza);
+
+alert(`My email address is $(email)`);
+
+alert(`I am trying to learn from the Book ${book}`);
+
+document.writeln("Yah! I can write HTML content through JavaScript");
+
+alert(end);
